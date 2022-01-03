b6c1023ae55b8b764c804f3a95a2ff41a0deca0a
QQQomputer <3477qq@gmail.com>
2022-01-03 23:24:27 +0900

    First commit

---
+++ bronze/src/com/bronze/s1/Q1.java
@@ -0,0 +1,28 @@
+package com.bronze.s1;
+
+import java.util.Scanner;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// H 24
+		// M 59
+		// m <15 => m+45
+		// m>15 => m-15 H+1
+		
+		int h=sc.nextInt();
+		int m=sc.nextInt();
+		
+		if(m>15) {
+			m -= 45;						
+		}else {
+			m += 15;
+			h -= 1;
+		}
+		System.out.print(h+" ");
+		System.out.println(m);
+
+	}
+
+}
